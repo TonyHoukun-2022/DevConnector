0ccde08b3fc1de25ca96b73cf857bbc30b1ebfc3
Tony Peng <66193941+tonypeng-93@users.noreply.github.com>
2022-02-01 23:36:31 +1100

    Delete .history directory

--- .history/server_20220130170851.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-//look for PORT variable in environment when deployed on Heroku
-const PORT = process.env.PORT || 
--- .history/server_20220130171016.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-//look for PORT variable in environment when deployed on Heroku || local port
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
